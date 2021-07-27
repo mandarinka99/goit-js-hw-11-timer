efb2ebd1e7cbfdd257ddb6a13b63e15a4282391a
mandarinka99 <alina.oksentiuk@gmail.com>
2021-07-27 20:10:29 +0300

    countdown timer

--- src/countdown-timer.js
+++ src/countdown-timer.js
@@ -1,27 +1,30 @@
 
 
-const CountdownTimer = function (obj) {
-  this.selector = document.querySelector(obj.selector);
-  this.targetDate = obj.targetDate;
-  
-  this.setTime = () => {
-    const currentTime = new Date;
-    const time = this.targetDate - currentTime;
-  
-    const days = Math.floor(time / (1000 * 60 * 60 * 24)).toString().padStart(2, 0);
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, 0);
-    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, 0);
-    const secs = Math.floor((time % (1000 * 60)) / 1000).toString().padStart(2, 0);
+class CountdownTimer {
+
+  constructor (obj) {
+    this.selector = document.querySelector(obj.selector);
+    this.targetDate = obj.targetDate;
+
+    this.setTime = () => {
+      const currentTime = new Date;
+      const time = this.targetDate - currentTime;
     
-    this.selector.querySelector('[data-value="days"]').innerHTML = `${days}`;
-    this.selector.querySelector('[data-value="hours"]').innerHTML = `${hours}`;
-    this.selector.querySelector('[data-value="mins"]').innerHTML = `${mins}`;
-    this.selector.querySelector('[data-value="secs"]').innerHTML = `${secs}`;
+      const days = Math.floor(time / (1000 * 60 * 60 * 24)).toString().padStart(2, 0);
+      const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, 0);
+      const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, 0);
+      const secs = Math.floor((time % (1000 * 60)) / 1000).toString().padStart(2, 0);
+      
+      this.selector.querySelector('[data-value="days"]').innerHTML = `${days}`;
+      this.selector.querySelector('[data-value="hours"]').innerHTML = `${hours}`;
+      this.selector.querySelector('[data-value="mins"]').innerHTML = `${mins}`;
+      this.selector.querySelector('[data-value="secs"]').innerHTML = `${secs}`;
+    }
+  
+    this.setTime();
+  
+    setInterval(this.setTime, 1000);
   }
-
-  this.setTime();
-
-  setInterval(this.setTime, 1000);
 };
 
 export default CountdownTimer;
